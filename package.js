e7d12b6eaa9aeeacced3226a981ce31b63b82e2b
kishigo <kkishigo@directv.com>
2015-12-16 08:18:48 -0800

    First commit

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+	name: 'kishigo:eventemitter-ex',
+	version: '0.0.1',
+	// Brief, one-line summary of the package.
+	summary: 'Wraps the listener callback with try-catch',
+	// URL to the Git repository containing the source code for this package.
+	git: '',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+	api.versionsFrom('1.2.1');
+	api.use('ecmascript');
+	api.use('raix:eventemitter');
+	api.export('EventEmitterEx');
+	api.addFiles('eventemitter-ex.js');
+});
+
+Package.onTest(function (api) {
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('kishigo:eventemitter-ex');
+	api.addFiles('eventemitter-ex-tests.js');
+});
